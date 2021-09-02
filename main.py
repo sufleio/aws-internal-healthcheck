d0c20d4000ea4aed92fbb6c1aa8742065b092e96
Burak Balta <burak@sufle.io>
2021-09-02 19:29:31 +0300

    pep8 update

--- main.py
+++ main.py
@@ -4,24 +4,24 @@ from botocore.vendored import urllib3
 http = urllib3.PoolManager()
 
 def lambda_handler(event, context):
-	#1. Parse out query string params
-	url = event['queryStringParameters']['url']
-	r = http.request('GET', url)
+    # 1. Parse out query string params
+    url = event['queryStringParameters']['url']
+    r = http.request('GET', url)
 
-	#2. Construct the body of the response object
-	transaction_response = {
-		'url': url, 
-		'message': r.status
-	}
+    # 2. Construct the body of the response object
+    transaction_response = {
+        'url': url,
+        'message': r.status
+    }
 
-	#3. Construct http response object
-	response_object = {
-		'headers': {
-			'Content-Type': 'application/json'
-		},
-		'statusCode': r.status,
-		'body': json.dumps(transaction_response)
-	}
+    # 3. Construct http response object
+    response_object = {
+        'headers': {
+            'Content-Type': 'application/json'
+        },
+        'statusCode': r.status,
+        'body': json.dumps(transaction_response)
+    }
 
-	#4. Return the response object
-	return response_object
+    # 4. Return the response object
+    return response_object
